a61509cd8481c50e005981ab4c76ed178b4d4eb0
sachinbedke <126045456+sachinbedke@users.noreply.github.com>
2024-04-30 21:04:16 +0530

    cimmjj

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config({ path: "./.env" })
 
 
